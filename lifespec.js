1f6706bc034a16dec44fe5801987c1f5f42877f2
Eric Dobbs <eric@dobbse.net>
2011-03-24 00:09:58 -0600

    replaced most of the code with a chain of map, reduce, and filter

--- lifespec.js
+++ lifespec.js
@@ -16,47 +16,23 @@
 	    return m == x ? row.filter(function(q) {return q[1] != y}) : row;
 	}).reduce(function (a, b) {return a.concat(b)}, []);
     }
-    function uniquePusherFor(arr) {
-	var seen = {};
-	return function (cell) {
-	    if (!seen[cell.join()]) {arr.push(cell);}
-	    seen[cell.join()] = 1;
-	}
-    }
-    function findCell(cell, generation) {
-	var result = false;
-	generation.map(function (address) {
-	    if (address.join() == cell.join()) {result = true;}
-	});
-	return result;
-    }
-    function fertileCellsFrom(generation) {
-	var fertileCells = [];
-	var pushUnique = uniquePusherFor(fertileCells);
-	generation.map(function (address) {
-	    pushUnique(address);
-	    (neighbors(address)).map(function (neighbor) {pushUnique(neighbor)});
-	});
-	return fertileCells;
-    }
-    function livingNeighbors(generation, cell) {
-	var sum = 0;
-	neighbors(cell).map(function (address) {
-	    if (findCell(address, generation)) {sum++;}
-	});
-	return sum;
-    }
-    function sprout(cell, generation) {
-	var count = livingNeighbors(generation, cell);
-	return (count == 3) || (count == 2 && findCell(cell, generation))
-    }
     function tick(generation) {
-	var nextGeneration = [];
-	var pushUnique = uniquePusherFor(nextGeneration);
-	(fertileCellsFrom(generation)).map(function (address) {
-	    if(sprout(address, generation)) {pushUnique(address);}
+	var seen = {}, alive = {};
+	return generation.map(function include_neighbors(address) {
+	    alive[address.join()] = 1;
+	    return neighbors(address);
+	}).reduce(
+	    function flatten (hood, the_neighbors) {return hood.concat(the_neighbors);},
+	    generation
+	).filter(function still_alive (address) {
+	    var k = address.join();
+	    if (seen[k]) {return false;}
+	    seen[k] = 1;
+	    var count = neighbors(address).reduce(function (a, b) {
+		return a + (alive[b.join()] || 0);
+	    }, 0);
+	    return count == 3 || (count == 2 && alive[address.join()]);
 	});
-	return nextGeneration;
     }
 describe("Conway's Game of Life", function () {
     describe("tick()", function () {
@@ -68,78 +44,6 @@ describe("Conway's Game of Life", function () {
 	    var VERTICAL_BLINKER = [[1,0], [1,1], [1,2]];
 	    var HORIZONTAL_BLINKER = [[0,1], [1,1], [2, 1]];
 	    expect(tick(VERTICAL_BLINKER).sort()).toEqual(HORIZONTAL_BLINKER.sort());
-	});
-    });
-    describe("uniqePusherFor", function () {
-	it("should return a function", function () {
-	    expect(typeof uniquePusherFor([])).toEqual('function');
-	});
-	it("should return a pusher that appends to the given array", function () {
-	    var arr = [];
-	    var pushUnique = uniquePusherFor(arr);
-	    pushUnique([1,1]);
-	    expect(arr.sort()).toEqual([[1,1]]);
-	    pushUnique([0,0]);
-	    expect(arr.sort()).toEqual([[0,0], [1,1]]);
-	});
-	it("should return a pusher that ignores duplicates", function () {
-	    var arr = [];
-	    var pushUnique = uniquePusherFor(arr);
-	    pushUnique([0,0]);
-	    expect(arr).toEqual([[0,0]]);
-	    pushUnique([0,0]);
-	    expect(arr).toEqual([[0,0]]);
-	});
-    });
-    describe("findCell(cell, generation)", function () {
-	it("should return true if the given cell is alive in the generation", function () {
-	    expect(findCell([0,0], [[1,1], [2,2], [0,0]])).toBeTruthy();
-	});
-	it("should return false if the given cell is not alive in the generation", function () {
-	    expect(findCell([0,0], [[1,1], [2,2]])).toBeFalsy();
-	});
-    });
-    describe("livingNeighbors(generation, cell)", function () {
-	it("should count the living neighbors of the cell in the given generation", function () {
-	    var generation = [[0,0], [0, 1], [0,2]];
-	    expect(livingNeighbors(generation, generation[0])).toEqual(1);
-	    expect(livingNeighbors(generation, generation[1])).toEqual(2);
-	});
-    });
-    describe("sprout(cell, generation) knows if a cell will sprout life in the next generation", function () {
-	var generation = [[1,0], [1,1], [1,2], [0,0]];
-	it("should be true if the number of living neighbors is three", function () {
-	    expect(sprout([2,1], generation)).toBeTruthy();
-	});
-	it("should be false if the number of living neighbors is not three", function () {
-	    expect(sprout([0,1], generation)).toBeFalsy();
-	});
-	it("should be true if I and two neighbors are alive", function () {
-	    expect(sprout([1,0], generation)).toBeTruthy();
-	});
-	it("should be false if two neighbors are alive but I am not", function () {
-	    expect(sprout([2,0], generation)).toBeFalsy();
-	});
-    });
-    describe("fertileCellsFrom(generation) knows where to look for sprouts", function () {
-	it("should return an empty set if this generation has no living cells", function () {
-	    var generation = [];
-	    expect(fertileCellsFrom(generation)).toEqual([]);
-	});
-	it("should include coordinates of the living cells in this generation", function () {
-	    var generation = [[0, 0]];
-	    var actualCells = fertileCellsFrom(generation);
-	    for (var i in generation) {
-		expect(actualCells).toContain(generation[i]);
-	    }
-	});
-	it("should include coordinates of the immediate neighbors of a living cell", function () {
-	    var generation = [[1,1]];
-	    var expectedCells = neighbors(generation[0]);
-	    var actualCells = fertileCellsFrom(generation);
-	    for (var i in expectedCells) {
-		expect(actualCells).toContain(expectedCells[i]);
-	    }
 	});
     });
     describe("neighbors(cellCoordinates)", function () {
